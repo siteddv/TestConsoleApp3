2996759f3c62401dcb1af35c71b08be7522925ee
dimitrii <siteddv@gmail.com>
2024-05-14 20:48:02 +0600

    corrected query

--- ConsoleApp1/Controller/PostgresController.cs
+++ ConsoleApp1/Controller/PostgresController.cs
@@ -5,33 +5,28 @@ namespace ConsoleApp1.Controller
 {
     public class PostgresController
     {
-       public const string ConnectionString = "User ID=postgres; Password=sa;" +
-       "Host=localHost; Port=5432; Database=postgres; Pooling=True;";
+       public const string ConnectionString = "User ID=new_superuser; Password=postgres;" +
+       "Host=localHost; Port=5432; Database=learn; Pooling=True;";
         public void GetDbInfo()
         {
-            using(DbConnection connection = new NpgsqlConnection(ConnectionString)) 
-            {
-                string query = "SELECT * FROM Superheroes " +
-                    "\r\nWHERE ID NOT IN(4710, 3670, 3535)" +
-                    "\r\nORDER BY NAME" +
-                    "\r\nLIMIT(10)";
+            using DbConnection connection = new NpgsqlConnection(ConnectionString);
+            string query = "SELECT name, align, universe FROM superheroes WHERE appearance_count > 100";
 
-                connection.Open();
+            connection.Open();
 
-                using (DbCommand command = connection.CreateCommand()) 
-                { 
-                    command.CommandText = query;
-                    using(DbDataReader reader = command.ExecuteReader())
+            using (DbCommand command = connection.CreateCommand()) 
+            { 
+                command.CommandText = query;
+                using(DbDataReader reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
                     {
-                        while(reader.Read())
-                        {
-                            for(int i = 1; i< reader.FieldCount; i++)
-                                Console.WriteLine($"{reader[i]}");
+                        for(int i = 1; i< reader.FieldCount; i++)
+                            Console.WriteLine($"{reader[i]}");
 
-                            Console.WriteLine() ;
-                        }
-
+                        Console.WriteLine() ;
                     }
+
                 }
             }
         }
